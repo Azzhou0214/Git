41ce9160c1f511ab7c416c77155ece35045296fa
Azhou <zzhou00214@gmail.com>
2023-10-26 22:58:49 +0800

    first commit by hot-fix

--- src/main/java/com/azz/test.java
+++ src/main/java/com/azz/test.java
@@ -5,5 +5,6 @@ public class test {
         System.out.println("测试而已");
         System.out.println("hello git!");
         System.out.println("hello github!");
+        System.out.println("hello github by hot-git!");
     }
 }
