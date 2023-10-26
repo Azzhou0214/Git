7dde3bd9a2675c307119795e84d13f036c1a0044
Azhou <zzhou00214@gmail.com>
2023-10-26 22:53:06 +0800

    second commit

--- src/main/java/com/azz/test.java
+++ src/main/java/com/azz/test.java
@@ -3,5 +3,7 @@ package com.azz;
 public class test {
     public static void main(String[] args) {
         System.out.println("测试而已");
+        System.out.println("hello git!");
+        System.out.println("hello github!");
     }
 }
